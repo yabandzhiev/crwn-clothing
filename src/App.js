8cfea52701586afb9197ea263e801d9daafff832
yabandzhiev <yabandzhiev@abv.bg>
2022-03-29 18:53:14 +0300

    move categories redux logic to shop route where it is needed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
 import Authentication from "./routes/auth/Authentication.jsx";
 import Home from "./routes/home/Home.jsx";
@@ -8,16 +7,13 @@ import Navigation from "./routes/navigation/Navigation.jsx";
 import Shop from "./routes/shop/Shop.jsx";
 import Checkout from "./routes/checkout/Checkout.jsx";
 import { setCurrentUser } from "./store/user/userActions.js";
-import { setCategoriesMap } from "./store/categories/categoryAction.js";
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-  getCategoriesAndDocuments,
 } from "./utils/firebase/firebase.js";
 
 const App = () => {
-  const dispatch = useDispatch();
-
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
@@ -29,14 +25,6 @@ const App = () => {
 
     return unsubscribe;
   }, [dispatch]);
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
-    };
-    getCategoriesMap();
-  }, []);
 
   return (
     <Routes>
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/CategoriesPreview.jsx";
 import Category from "../category/Category.jsx";
+import { setCategoriesMap } from "../../store/categories/categoryAction.js";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.js";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoriesMap();
+  }, []);
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
