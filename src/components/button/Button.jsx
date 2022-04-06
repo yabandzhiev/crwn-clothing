fbeb428769333ce4b1f593964508a1ae5c53ebbe
yabandzhiev <yabandzhiev@abv.bg>
2022-04-06 09:47:42 +0300

    add loading spinner button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,4 +1,9 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from "./Button.styles.jsx";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+  ButtonSpinner,
+} from "./Button.styles.jsx";
 
 export const BUTTON_TYPE_CLASSES = {
   base: "base",
@@ -13,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
--- src/components/button/Button.styles.jsx
+++ src/components/button/Button.styles.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { SpinnerContainer } from "../spinner/Spinner.styles.jsx";
+
 export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
@@ -17,7 +19,7 @@ export const BaseButton = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
-
+  align-items: center;
   &:hover {
     background-color: white;
     color: black;
@@ -45,4 +47,9 @@ export const InvertedButton = styled(BaseButton)`
     color: white;
     border: none;
   }
+`;
+
+export const ButtonSpinner = styled(SpinnerContainer)`
+  width: 30px;
+  height: 30px;
 `;
