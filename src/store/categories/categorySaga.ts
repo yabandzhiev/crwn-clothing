b43ff31471b765367d430561b62ca7cfa89becbc
yabandzhiev <yabandzhiev@abv.bg>
2022-04-12 10:57:04 +0300

    refactor categorysaga to ts

--- src/store/categories/categorySaga.ts
+++ src/store/categories/categorySaga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
 
@@ -8,17 +8,17 @@ import { CATEGORIES_TYPES } from "./categoryTypes";
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
-  yield takeLatest(CATEGORIES_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(CATEGORIES_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
