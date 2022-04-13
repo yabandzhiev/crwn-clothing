1972627bb2e7e2eabac07557a37b5e7fc4b0d20d
yabandzhiev <yabandzhiev@abv.bg>
2022-04-13 09:33:20 +0300

    rootSaga to Typescript

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,8 +1,8 @@
-import { all, call } from "redux-saga/effects";
+import { all, call } from "typed-redux-saga/macro";
 
 import { categoriesSaga } from "./categories/categorySaga";
 import { userSagas } from "./user/userSaga";
 
 export function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSagas)]);
+  yield* all([call(categoriesSaga), call(userSagas)]);
 }
